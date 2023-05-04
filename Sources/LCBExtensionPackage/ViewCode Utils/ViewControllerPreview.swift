af2494246676e9481b387318a5e4b5936bdf869c
Lucas C Barros <Lucas_cb@icloud.com>
2023-05-03 23:24:27 -0700

    Config

--- Sources/LCBExtensionPackage/ViewCode Utils/ViewControllerPreview.swift
+++ Sources/LCBExtensionPackage/ViewCode Utils/ViewControllerPreview.swift
@@ -8,18 +8,20 @@
 import UIKit
 import SwiftUI
 
-struct ViewControllerPreview: UIViewControllerRepresentable {
+public struct ViewControllerPreview: UIViewControllerRepresentable {
     public let viewControllerBuilder: () -> UIViewController
 
     public init(_ viewControllerBuilder: @escaping () -> UIViewController) {
         self.viewControllerBuilder = viewControllerBuilder
     }
     
-    func makeUIViewController(context: Context) -> some UIViewController {
+    public func makeUIViewController(context: Context) -> some UIViewController {
         return viewControllerBuilder()
     }
+}
 
-    func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
+extension UIViewControllerRepresentable {
+    public func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
         // Not needed
     }
 }
